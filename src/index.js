9082aed0e5614959fe5ededd185ea9bfc6faabf2
Vaya <vaya.valcheva@icloud.com>
2023-10-07 15:06:59 +0300

    Vaya's first commit ever

---
+++ src/index.js
@@ -0,0 +1,14 @@
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello Niki");
+console.log("hello monka");
+
+let age = 21
+if ( age >= 18 ){
+    console.log ("pulnoleten")     
+} else {
+    console.log("nepolnoleten")
+
+}
+
