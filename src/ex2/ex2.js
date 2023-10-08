3d2a6d1c88fac518900b804431a69b2f4ebcf18f
Vaya <vaya.valcheva@icloud.com>
2023-10-08 12:25:02 +0300

    ex2

--- src/ex2/ex2.js
+++ src/ex2/ex2.js
@@ -1,3 +1,11 @@
-function degreeToRadians (radian) {
+function raadiansToDegrees (radian) {
     return radian * 180 / Math.PI
 }
+
+function degreesToRadians (degrees) {
+    return degrees / 180 / Math.PI
+
+
+}
+
+console.log (degreesToRadians(140))
